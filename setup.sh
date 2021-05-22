e2071ae26e0d5d2fa43a65eb8548311728ce0e67
Ayangbile Daniel Bayo <65937766+DanielBayo@users.noreply.github.com>
2021-05-22 22:22:37 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,9 @@
-mkdir -p ~/.streamlit/
-echo "\
-[general]\n\
-email = \"your-email@domain.com\"\n\
-" > ~/.streamlit/credentials.toml
-echo "\
+ral]\n\
+email = \”danielayangbile@gmail.com\”\n\
+“ > ~/.streamlit/credentials.toml
+echo “\
 [server]\n\
 headless = true\n\
 enableCORS=false\n\
 port = $PORT\n\
-" > ~/.streamlit/config.toml
+“ > ~/.streamlit/config.toml
